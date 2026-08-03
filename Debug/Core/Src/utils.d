6facb5dd9c73ccabcbea789e339f5c398382a2f4
NikunSinghal <108673437+NikunSinghal@users.noreply.github.com>
2026-08-03 19:52:22 +0530

    added gitignore

--- Debug/Core/Src/utils.d
+++
@@ -1,5 +0,0 @@
-Core/Src/utils.o: ../Core/Src/utils.c ../Core/Inc/utils.h \
- ../Core/Inc/sensor_module.h ../Core/Inc/types.h
-../Core/Inc/utils.h:
-../Core/Inc/sensor_module.h:
-../Core/Inc/types.h:
